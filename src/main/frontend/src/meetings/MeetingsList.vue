7be8f33ae32e6e568d077c4ee3db8078f2a552fe
marcin7777 <marcinmz9@tlen.pl>
2020-05-29 14:09:02 +0200

    Addaed changes in tbody

--- src/main/frontend/src/meetings/MeetingsList.vue
+++ src/main/frontend/src/meetings/MeetingsList.vue
@@ -1,17 +1,21 @@
 <template>
+
+
   <table v-if="meetings.length > 0">
     <thead>
     <tr>
       <th>Nazwa spotkania</th>
       <th>Opis</th>
+      <th>data</th>
       <th>Uczestnicy</th>
       <td></td>
     </tr>
     </thead>
     <tbody>
-    <tr v-for="meeting in meetings" :key="meeting.name">
-      <td>{{ meeting.name }}</td>
+    <tr v-for="meeting in meetings" :key="meeting.id">
+      <td>{{ meeting.title }}</td>
       <td>{{ meeting.description }}</td>
+      <td>{{ meeting.date }}</td>
       <td>
         <ul v-if="meeting.participants">
           <li v-for="participant in meeting.participants" :key="participant">
@@ -20,22 +24,30 @@
         </ul>
       </td>
       <td style="text-align: right; min-width: 400px">
-        <button v-if="meeting.participants.indexOf(username) < 0" class="button-outline"
+        <button v-if="!isEnrolled(username, meeting)" class="button-outline"
                 @click="$emit('attend', meeting)">
           Zapisz się
         </button>
-        <button v-else class="button-outline" @click="$emit('unattend', meeting)">Wypisz się</button>
-        <button v-if="meeting.participants.length === 0" class="button" @click="$emit('delete', meeting)">
+        <button v-if="isEnrolled(username, meeting)" class="button-outline" @click="$emit('unattend', meeting)">Wypisz się</button>
+        <button v-if="meeting.participants == null ||  meeting.participants.length == 0 " class="button" @click="$emit('delete', meeting)">
           Usuń puste spotkanie
         </button>
       </td>
     </tr>
     </tbody>
   </table>
-</template>
+</template>-->
 
 <script>
     export default {
-        props: ['meetings', 'username']
+        props: ['meetings', 'username'],
+      methods: {
+          isEnrolled(username, meeting){
+            for (let participant in meeting.participants){
+              if(participant.name === "username") return true;
+            }
+            return false;
+          }
+      }
     }
 </script>
